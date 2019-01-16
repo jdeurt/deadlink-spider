2a81f6248f34988312c32990604628f1e0ff9368
Juan de Urtubey <jdeurt@gmail.com>
2019-01-15 18:23:58 -0600

    Added logging on graceful exit.

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const linkChecker = new Spider(process.env.DOMAIN, true);
 
 linkChecker.start();
 
+process.on("exit", () => {
+    linkChecker.stop();
+    fs.writeFileSync(`./output/crawl-path_${Date.now()}.json`, JSON.stringify(linkChecker.crawlPath));
+    fs.writeFileSync(`./output/report_${Date.now()}.txt`, `OK: ${linkChecker.report.ok.length}\n${linkChecker.report.ok.join("\n")}\n\nWARN: ${linkChecker.report.warn.length}\n${linkChecker.report.warn.join("\n")}\n\nERROR: ${linkChecker.report.error.length}\n${linkChecker.report.error.join("\n")}`);
+    process.exit();
+});
+
 process.on("SIGINT", () => {
     linkChecker.stop();
     fs.writeFileSync(`./output/crawl-path_${Date.now()}.json`, JSON.stringify(linkChecker.crawlPath));
