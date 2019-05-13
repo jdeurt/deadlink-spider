cb19283603ee4e04eaafa097a28e535e036713cf
Juan de Urtubey <jdeurt@gmail.com>
2019-05-13 13:42:54 -0500

    Fix.

--- index.js
+++ index.js
@@ -12,20 +12,18 @@ if (process.argv.length > 2) startURL = process.argv[2];
 if (process.argv.length > 3) blacklist = process.argv.slice(3);
 
 if (startURL.match(/[^\/]\/[^\/]/)) linkChecker = new SitemapSpider(startURL, blacklist, false);
-else linkChecker = new Spider(startURL, blacklist, true);
+else linkChecker = new Spider(startURL, blacklist);
 
 linkChecker.start();
 
 process.on("exit", () => {
     linkChecker.stop();
-    fs.writeFileSync(`./output/crawl-path_${startURL.replace(/\//g, "")}_${Date.now()}.json`, JSON.stringify(linkChecker.crawlPath));
     fs.writeFileSync(`./output/report_${startURL.replace(/\//g, "")}_${Date.now()}.txt`, formatReport(linkChecker.report, startURL));
     process.exit();
 });
 
 process.on("SIGINT", () => {
     linkChecker.stop();
-    fs.writeFileSync(`./output/crawl-path_${startURL.replace(/\//g, "")}_${Date.now()}.json`, JSON.stringify(linkChecker.crawlPath));
     fs.writeFileSync(`./output/report_${startURL.replace(/\//g, "")}_${Date.now()}.txt`, formatReport(linkChecker.report, startURL));
     process.exit();
 });
